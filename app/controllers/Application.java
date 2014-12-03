240543c806eecd4c0bf4aaf5621f604b2eddf0de
Vinicius Vendramini <vinivendra@gmail.com>
2014-12-03 19:08:26 -0200

    PRE-PARA QUE AGORA É HORA DO MERGE GIROMBOSO

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,3 +1,25 @@
+/*
+ * Application.java
+ * 
+ * Version 1.0
+ *
+ * 02/12/2014
+ * 
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *                    Version 2, December 2004 
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified 
+ * copies of this license document, and changing it is allowed as long 
+ * as the name is changed. 
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
 package controllers;
 
 import java.util.List;
@@ -9,24 +31,32 @@ import play.mvc.*;
 import views.html.*;
 import models.*;
 
+/** 
+ * The Application class is the main Controller. It is used to control the main 
+ * flow of the application and to haldle callbacks from buttons.
+ */
+public class Application extends Controller  {
 
-public class Application extends Controller 
-{
-
-    public static Result index()
-    {
+    /** 
+     * The main method called when the application starts.
+     */
+    public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
     
-    public static Result criaHabilidadesIniciais()
-    {
-    	
+    /** 
+     * Creates a set of default initial Skills to display at the shop.
+     */
+    public static Result criaHabilidadesIniciais() {
     	List<Habilidade> habilidades = Habilidade.find.all();
-    	if(habilidades.size() != 0) return ok(index.render("Já existem habilidades no BD"));
+        Habilidade h;
+
+    	if (habilidades.size() != 0) {
+            return ok(index.render("Já existem habilidades no BD"));
+        }
     	
-    	Habilidade h = new Habilidade("Patada", 10, 10);
+    	h = new Habilidade("Patada", 10, 10);
     	h.save();
-    	
     	
     	h = new Habilidade("Patada inteligente", 10, 20);
     	h.setMinWis(10);
@@ -36,68 +66,85 @@ public class Application extends Controller
     	h.setMinStr(50);
     	h.save();
     	
-    	
     	return ok(index.render("Habilidades criadas"));
     }
     
-    
-    public static Result shop(String id)
-    {
-    	Monstro mon;
-    	mon = Monstro.find.byId(id);
-    	List<Habilidade> habilidades = Habilidade.find.all();
+    /** 
+     * Executes the purchase of a skill, updates the database and the local 
+     * objects.
+     */
+    public static Result compraDeHabilidade() {
+        DynamicForm data = Form.form().bindFromRequest();
+        Habilidade h = Habilidade.find.byId(data.get("habId"));
+        Monstro mon = Monstro.find.byId(data.get("mId"));
+
+        mon.compraHabilidade(h);
+        mon.save();
+
+        return redirect("/monstro/"+data.get("mId")+"/shop");
+    }
+
+    /** 
+     * The method used to render the shop.
+     */
+    public static Result shop(String id) {
+    	Monstro mon = Monstro.find.byId(id);
+        List<Habilidade> habilidades = Habilidade.find.all();
+
     	return ok(loja.render(mon, habilidades));
-    	
     }
     
-    public static Result compraDeHabilidade()
-    {
-    	DynamicForm data = Form.form().bindFromRequest();
-    	Monstro	mon = Monstro.find.byId(data.get("mId"));
-    	Habilidade h = Habilidade.find.byId(data.get("habId"));
-    	mon.compraHabilidade(h);
+    /** 
+     * Renders a test screen.
+     */
+    public static Result teste1() {
+    	Monstro mon = new Monstro("Leo Stronda");
+
     	mon.save();
-    	return redirect("/monstro/"+data.get("mId")+"/shop");
+
+    	return ok(teste.render(mon));
     }
-    
-    
-    public static Result teste1()
-    {
-    	Monstro jubs = new Monstro("Leo Stronda");
-    	jubs.save();
-    	return ok(teste.render(jubs));
-    }
-    
-    public static Result absorve()
-    {
+
+    /** 
+     * Executes the absorption of energy from the pool to the monster.
+     */
+    public static Result absorve() {
     	DynamicForm dynamicForm = Form.form().bindFromRequest();
-    	Monstro mon;
-    	mon = Monstro.find.byId(dynamicForm.get("mId"));
+    	Monstro mon = Monstro.find.byId(dynamicForm.get("mId"));
+
     	mon.somaEnergia(Integer.parseInt(dynamicForm.get("pontos")));
     	mon.save();
+
     	return redirect("/monstro/"+dynamicForm.get("mId"));
     }
     
-    public static Result mostraMonstro(String id)
-    {
-    	Monstro mon;
-    	mon = Monstro.find.byId(id);
+    /** 
+     * Renders the monster.
+     */
+    public static Result mostraMonstro(String id) {
+    	Monstro mon = Monstro.find.byId(id);;
+
     	return ok(teste.render(mon));
     }
     
-    public static Result treina(String tipo)
-    {
+    /** 
+     * Attempts to train the Monster's given attribute (must check if the
+     * Monster is able to train that attribute first).
+     */
+    public static Result treina(String tipo) {
     	DynamicForm data = Form.form().bindFromRequest();
     	Monstro	mon = Monstro.find.byId(data.get("mId"));
-    	if(tipo.equals("str"))
+
+    	if (tipo.equals("str")) {
     		mon.treinaAtributo(Monstro.Atributo.STR);
-    	else if(tipo.equals("dex"))
+        } else if (tipo.equals("dex")) {
     		mon.treinaAtributo(Monstro.Atributo.DEX);
-    	else if(tipo.equals("wis"))
+    	} else if (tipo.equals("wis")) {
     		mon.treinaAtributo(Monstro.Atributo.WIS);
+        }
+
     	mon.save();
     	
     	return redirect("/monstro/"+data.get("mId"));
     }
-
 }
--- app/models/Monstro.java
+++ app/models/Monstro.java
@@ -1,5 +1,26 @@
-package models;
+/*
+ * Monstro.java
+ * 
+ * Version 1.0
+ *
+ * 02/12/2014
+ * 
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *                    Version 2, December 2004 
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified 
+ * copies of this license document, and changing it is allowed as long 
+ * as the name is changed. 
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
 
+package models;
 
 import play.db.ebean.Model;
 
@@ -8,34 +29,63 @@ import javax.persistence.*;
 import models.Habilidade;
 import java.util.*;
 
+
+/** 
+ * The Monster class represents a Monster that a player may use, which is 
+ * the main form of interaction of the player with the game world.
+ */
 @Entity
 public class Monstro extends Model {
-
+    /** 
+     * The Attribute enum is used to execute operations regarding the Monster's
+     * individual attributes with ease. The three values (STR, DEX and WIS) 
+     * represent Strength, Dexerity and Wisdom respectively.
+     */
     public static enum Atributo {
         STR, DEX, WIS
     }
-    
-	/**
-	 
-	private static final long serialVersionUID = 3799161690136050003L;
-	* 
-	 */
 
+    /** 
+     * A Finder object used to fetch Skills from the database.
+     */
+    public static Finder<String,Monstro> find = 
+            new Finder<String,Monstro>(String.class, Monstro.class); 
+    /** 
+     * The id is used by the database to reference each Monster individually.
+     */
 	@Id
 	public String id;
-	
+
+    /** 
+     * The Inventory contains all Skills that have been purchased for a specific
+     * Monster and therefore may be used by that Monster.
+     */
 	@ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	public List<Habilidade> inventario = new ArrayList<Habilidade>();
-	
+
+    /** 
+     * The Monster's name, which may be displayed at the top of the screen.
+     */
 	public String nome;
+    /** 
+     * The Monster's current stored energy points.
+     */
 	public int energia;
-	public int dex, str, wis;
+    /** 
+     * The Dexerity attribute.
+     */
+	public int dex;
+    /** 
+     * The Strength attribute.
+     */
+    public int str;
+    /** 
+     * The Wisdom attribute.
+     */
+    public int wis;
 	
-	
-	
-	
-	public Monstro(String nome)
-	{
+	// Constructor
+	public Monstro(String nome) {
 		super();
 		this.nome = nome;
 		this.energia = 0;
@@ -43,15 +93,18 @@ public class Monstro extends Model {
 		this.str = 1;
 		this.wis = 1;
 	}
-
-	public static Finder<String,Monstro> find = new Finder<String,Monstro>(String.class, Monstro.class); 
-
 	
-	
-	public boolean compraHabilidade(Habilidade h)
-	{
-		if(this.podeComprar(h))
-		{
+    // Skills
+    /** 
+     * Checks if the Monster is able to buy the given Skill. If it is, 
+     * the Skill is purchased (the cost is subtracted from the Monster's
+     * energy pool) and the Skill is added to the inventory.
+     * @param skill The desired Skill to purchase.
+     * @return @p true if the purchase was successfully completed, @p false
+     * otherwise.
+     */
+	public boolean compraHabilidade(Habilidade h) {
+		if (this.podeComprar(h)) {
 			this.setEnergia(this.getEnergia() - h.getCusto());
 			this.inventario.add(h);
 			return true;
@@ -59,66 +112,109 @@ public class Monstro extends Model {
 		return false;
 	}
 	
-	
-	public boolean podeComprar(Habilidade h)
-	{
-		if(this.getDex() < h.getMinDex() || this.getStr() < h.getMinStr() || this.getWis() < h.getMinWis() || this.getEnergia() < h.getCusto())
+    /** 
+     * Check if the Monster is able to buy the Skill (if it has the necessary
+     * attributes and energy).
+     * @param skill The desired Skill to purchase.
+     * @return @p true if the purchase is possible, @p false otherwise.
+     */
+	public boolean podeComprar(Habilidade h) {
+		if (this.getDex() < h.getMinDex()
+                || this.getStr() < h.getMinStr()
+                || this.getWis() < h.getMinWis() 
+                || this.getEnergia() < h.getCusto()) {
 			return false;
-		if(temHabilidade(h))
+        }
+		if (temHabilidade(h)) {
 			return false;
+        }
 		return true;
 	}
 	
-	public boolean temHabilidade(Habilidade h)
-	{
-	    for(Habilidade h2 : this.getInventario())
-	        if(h2.getId() == h.getId()) return true;
+    /** 
+     * Check if the Monster has the given skill in its inventory/
+     * @param skill The skill to check.
+     * @return @p true if the skill is present, @p false otherwise.
+     */
+	public boolean temHabilidade(Habilidade h) {
+	    for (Habilidade h2 : this.getInventario()) {
+	        if (h2.getId() == h.getId()) {
+                return true;
+            }
+        }
 	    return false;
 	}
 	
-	public List<Habilidade> getInventario()
-	{
+    // Attributes
+    /** 
+     * Checks if the Mosnter has enough energy to increase the desired 
+     * attribute. If it does, the attribute is increased.
+     * @param attribute A value of the @p Attribute enum representing the 
+     * attribute to increase.
+     */
+    public void treinaAtributo(Atributo atributo) {
+        int custo = this.custoAtributo(atributo);
+    
+        if(this.energia >= custo) {
+            this.energia -= custo;
+            this.incrementaAtributo(atributo);
+        }
+    }
+    
+    /** 
+     * Returns the cost of increasing the Monster's given attribute.
+     * @param attribute A value of the @p Attribute enum representing the 
+     * attribute whose cost is desired.
+     */
+    public int custoAtributo(Atributo atributo) {
+        return this.getAtributo(atributo) * this.getAtributo(atributo);
+    }
+    /** 
+     * Increments the Monster's given @p attribute by one. This method assumes
+     * the Monster is allowed to increase the attribute, but does not require 
+     * it.
+     */
+    private void incrementaAtributo(Atributo atributo) {
+        this.setAtributo(atributo, this.getAtributo(atributo) + 1);
+    }
+
+    // Energy
+    /** 
+     * Takes all the energy available in the energy pool and transfers it to the
+     * Monster's own storage.
+     */
+    public void somaEnergia(int energia) {
+        this.setEnergia(this.getEnergia() + energia);
+    }
+
+    // Getters and Setters
+	public List<Habilidade> getInventario() {
 		return inventario;
 	}
 	
-	
-	public String getId()
-	{
+	public String getId() {
 		return this.id;
 	}
-	
-	public int getDex() {
-		return this.dex;
-	}
-	public void setDex(int dex) {
-		this.dex = dex;
-	}
-	
-	public void treinaAtributo(Atributo atributo)
-	{
-        int custo = this.custoAtributo(atributo);
+
+    public String getNome() {
+        return this.nome;
+    }
     
-		if(this.energia >= custo)
-		{
-			this.energia -= custo;
-			this.incrementaAtributo(atributo);
-		}
-	}
+    public int getEnergia() {
+        return this.energia;
+    }
     
-	public int custoAtributo(Atributo atributo)
-	{
-		return this.getAtributo(atributo) * this.getAtributo(atributo);
-	}
-	
-    public void incrementaAtributo(Atributo atributo)
-    {
-        int valorAntigo = this.getAtributo(atributo);
-        int valorNovo = valorAntigo + 1;
-        this.setAtributo(atributo, valorNovo);
+    public void setEnergia(int energia) {
+        this.energia = energia;
     }
 
-    public int getAtributo(Atributo atributo)
-    {
+    /** 
+     * @param attribute A value of the @p Attribute enum representing the 
+     * attribute to get.
+     * @return The value of the Monster's appropriate @p attribute.
+     * This method throws an exception if the attribute is unknown.
+     */
+    public int getAtributo(Atributo atributo) {
         switch (atributo) {
             case STR:
                 return this.getStr();
@@ -131,6 +227,12 @@ public class Monstro extends Model {
         }
     }
     
+    /** 
+     * @param attribute A value of the @p Attribute enum representing the 
+     * attribute to set.
+     * @param value The new value that the Monster's attribute should have.
+     * This method throws an exception if the attribute is unknown.
+     */
     public void setAtributo(Atributo atributo, int valor)
     {
         switch (atributo) {
@@ -149,45 +251,25 @@ public class Monstro extends Model {
         }
     }
 
+    private int getDex() {
+        return dex;
+    }
+    private void setDex(int dex) {
+        this.dex = dex;
+    }
 
-	public int getStr() {
-		return str;
-	}
-	public void setStr(int str) {
-		this.str = str;
-	}
+    private int getStr() {
+        return str;
+    }
+    private void setStr(int str) {
+        this.str = str;
+    }
 
-	public int getWis() {
-		return wis;
-	}
-	public void setWis(int wis) {
-		this.wis = wis;
-	}
-
-	public String getNome()
-	{
-		return this.nome;
-	}
-	
-	/*public void setNome(String nome)
-	{
-		this.nome = nome;
-	}*/
-	
-	public int getEnergia()
-	{
-		return this.energia;
-	}
-	
-	public void setEnergia(int energia)
-	{
-		this.energia = energia;
-	}
-	
-	public void somaEnergia(int energia)
-	{
-        int novaEnergia = this.getEnergia() + energia;
-        this.setEnergia(novaEnergia);
-	}
+    private int getWis() {
+        return wis;
+    }
+    private void setWis(int wis) {
+        this.wis = wis;
+    }
 
 }
